7c5d812cf99347b9390747d80ccb4e7c99e46af1
Aman Singh <work.amansingh12@gmail.com>
2026-07-25 10:11:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> minAnd2ndMin(vector<int> &arr) {
+        // code here
+        int first=INT_MAX;
+        int second=INT_MAX;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] < first){
+                second=first;
+                first=arr[i];
+            }
+            else if(arr[i] < second && arr[i] > first){
+                second=arr[i];
+            } 
+         
+        
+        }
+        
+        if(first == INT_MAX || second == INT_MAX){
+            return {-1};
+        }
+        
+        return {first,second};
+        
+        
+    }
+};
